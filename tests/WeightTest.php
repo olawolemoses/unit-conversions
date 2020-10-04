20ae143ff9d270f690ec53d93daf12ae845b948c
olawolemoses <olawolemoses@gmail.com>
2020-10-04 06:22:49 +0100

    fixed tests

---
+++ tests/WeightTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Freemind\UnitConversions\Tests;
+use Freemind\UnitConversions\Weight;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function it_can_convert_kilograms_to_lbs()
+    {
+        $lbs = Weight::fromKilograms(100)->toLbs();
+
+        // echo $lbs;
+
+        $this->assertEquals(220.4623, $lbs);
+    }
+
+
+}
